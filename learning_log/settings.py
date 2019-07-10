6cc4c1003ede38d8e55e8f866233772f12a1e050
Jalani Paul <jalani2727@gmail.com>
2019-07-09 21:41:06 -0400

    registered Topic model

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # My applications
+    'learning_logs'
 ]
 
 MIDDLEWARE = [
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Topics(models.Model):
+class Topic(models.Model):
     # A topic that the user is learning about.
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)

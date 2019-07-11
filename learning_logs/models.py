14e4493d76cf1b1744659c065e1f66d3ab35c156
Jalani Paul <jalani2727@gmail.com>
2019-07-11 08:45:34 -0400

    added entry model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,3 +9,16 @@ class Topic(models.Model):
         # Return a string representation of the model.
         return self.text
 
+class Entry(models.Model):
+    # Some details about the topic you've created.
+    topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        # Return the first 50 characters followed by "..."
+        return self.text[:50] + "..."
+
